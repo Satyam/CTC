02f5474324f296cd16b431e97fcb7046ea0a9d53
Satyam <satyam@satyam.com.ar>
2015-04-16 00:20:05 +0200

    Using separate LESS style sheets merged into a single style file

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,9 @@ module.exports = function (grunt) {
 				dest: 'dist/<%= pkg.name %>.js'
 			},
 			options: {
-			  transform: [['babelify', {sourceMap:true}]]
+				transform: [['babelify', {
+					sourceMap: true
+						}]]
 			}
 		},
 		jshint: {
@@ -41,9 +43,27 @@ module.exports = function (grunt) {
                 }]
 			}
 		},
+		less: {
+			main: {
+				files: [{
+					expand: true, // Enable dynamic expansion.
+					cwd: 'js/', // Src matches are relative to this path.
+					src: ['**/*.less'], // Actual pattern(s) to match.
+					dest: 'tmp/', // Destination path prefix.
+					ext: '.css', // Dest filepaths will have this extension.
+                }]
+			}
+		},
+		concat: {
+			main: {
+				files: {
+					'dist/estilos.css': ['css/*.css', 'tmp/**/*.css']
+				}
+			}
+		},
 		copy: {
 			main: {
-				expand:true,
+				expand: true,
 				cwd: 'tmp',
 				src: '*.js.map',
 				dest: 'dist/',
@@ -52,18 +72,28 @@ module.exports = function (grunt) {
 		watch: {
 			scripts: {
 				files: 'js/**/*.js',
-				tasks: ["default"]
+				tasks: ["js"]
+			},
+			styles: {
+				files: 'js/**/*.less',
+				tasks: ['css']
 			}
+
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-babel');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint', 'browserify']);
+	grunt.registerTask('default', ['jshint', 'browserify', 'less','concat']);
+	grunt.registerTask('js', ['jshint', 'browserify']);
+	grunt.registerTask('css', ['less', 'concat']);
 
 };
